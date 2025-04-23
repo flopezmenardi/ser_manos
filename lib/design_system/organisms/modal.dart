4ef643b55ab80e43205aaaa2b343db3da76cc785
Manuel Dithurbide <manudithurbide@teclaacademy.com>
2025-04-23 16:40:09 -0300

    modal

---
+++ lib/design_system/organisms/modal.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:ser_manos/design_system/molecules/buttons/text_button.dart';
+
+class ModalSermanos extends StatelessWidget {
+  final String title;
+  final String subtitle;
+  final String confimationText;
+  final String cancelText;
+  final VoidCallback onConfirm;
+  final VoidCallback onCancel;
+
+  const ModalSermanos({
+    super.key,
+    required this.title,
+    required this.subtitle,
+    required this.confimationText,
+    required this.cancelText,
+    required this.onConfirm,
+    required this.onCancel,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(title, style: Theme.of(context).textTheme.bodyLarge),
+          const SizedBox(height: 4),
+          Text(
+            subtitle,
+            style: Theme.of(
+              context,
+            ).textTheme.headlineSmall?.copyWith(fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 24),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              TextOnlySermanosButton(text: cancelText, onPressed: onCancel),
+              TextOnlySermanosButton(text: confimationText, onPressed: onConfirm)
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
