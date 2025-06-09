370a9664ea574b1cdb96c611f39a62ba10894b71
Felix Lopez <flopezmenardi@itba.edu.ar>
2025-06-09 14:44:58 -0300

    Add crashlytics

--- lib/features/auth/controllers/register_controller.dart
+++ lib/features/auth/controllers/register_controller.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_auth/firebase_auth.dart' as fb_auth;
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:ser_manos/providers/user_provider.dart';
 
@@ -29,7 +31,25 @@ class RegisterController {
       final uid = userCredential.user!.uid;
 
       final firestore = ref.read(firestoreServiceProvider);
-      await firestore.createUser(uid, nombre, apellido, email);
+      try {
+        await firestore.createUser(uid, nombre, apellido, email);
+        FirebaseAnalytics.instance.logEvent(
+          name: 'user_creation_success',
+          parameters: {'source': 'RegisterController'},
+        );
+      } catch(e, stack) {
+          FirebaseCrashlytics.instance.recordError(
+          e,
+          stack,
+          reason: 'Failed to create user document in Firestore',
+          fatal: false,
+        );
+        FirebaseAnalytics.instance.logEvent(
+          name: 'user_creation_failed',
+          parameters: {'source': 'RegisterController'},
+        );
+        rethrow;
+      }
 
       final user = await firestore.getUserById(uid);
       if (user != null) {
