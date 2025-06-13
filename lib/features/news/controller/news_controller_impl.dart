fa11c0d311a63aa1156baa7f0d559e77499fa232
jgirod <joaquingirod@gmail.com>
2025-06-12 22:15:23 -0300

    Minor refactor in news

--- lib/features/news/controller/news_controller_impl.dart
+++ lib/features/news/controller/news_controller_impl.dart
@@ -10,6 +10,19 @@ final newsControllerProvider = Provider<NewsController>((ref) {
   return NewsControllerImpl(newsService);
 });
 
+final newsListNotifierProvider = StateNotifierProvider<NewsListNotifier, AsyncValue<List<News>>>((ref) {
+  final controller = ref.read(newsControllerProvider);
+  return NewsListNotifier(controller);
+});
+
+final newsDetailNotifierProvider = StateNotifierProvider.family<NewsDetailNotifier, AsyncValue<News?>, String>((
+  ref,
+  newsId,
+) {
+  final controller = ref.read(newsControllerProvider);
+  return NewsDetailNotifier(controller, newsId);
+});
+
 class NewsControllerImpl implements NewsController {
   final NewsService _newsService;
 
@@ -21,5 +34,40 @@ class NewsControllerImpl implements NewsController {
 
   Future<News?> getNewsById(String id) {
     return _newsService.getNewsById(id);
+  }
+}
+
+class NewsListNotifier extends StateNotifier<AsyncValue<List<News>>> {
+  NewsListNotifier(this._controller) : super(const AsyncValue.loading()) {
+    fetchNews();
+  }
+
+  final NewsController _controller;
+
+  Future<void> fetchNews() async {
+    try {
+      final data = await _controller.getNewsOrderedByDate();
+      state = AsyncValue.data(data);
+    } catch (e, st) {
+      state = AsyncValue.error(e, st);
+    }
+  }
+}
+
+class NewsDetailNotifier extends StateNotifier<AsyncValue<News?>> {
+  NewsDetailNotifier(this._controller, this.newsId) : super(const AsyncValue.loading()) {
+    fetchNewsDetail();
+  }
+
+  final NewsController _controller;
+  final String newsId;
+
+  Future<void> fetchNewsDetail() async {
+    try {
+      final news = await _controller.getNewsById(newsId);
+      state = AsyncValue.data(news);
+    } catch (e, st) {
+      state = AsyncValue.error(e, st);
+    }
   }
 }
