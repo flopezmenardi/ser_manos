203d5db1b4010792b565e05f23c16ea24b79a7c2
Felix Lopez <flopezmenardi@itba.edu.ar>
2025-06-14 19:31:21 -0300

    Delete profile folder

--- lib/features/profile/controller/profile_controller.dart
+++
@@ -1,8 +0,0 @@
-import 'package:image_picker/image_picker.dart';
-import '../../../models/user_model.dart';
-
-abstract class ProfileController {
-  Future<User> getUserById(String uid);
-  Future<void> updateUser(String uid, Map<String, dynamic> data);
-  Future<void> uploadProfilePicture(String uid, XFile xfile);
-}
